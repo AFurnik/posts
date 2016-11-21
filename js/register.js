c40c01edae0c0ff2fa4485a1571799e343d490ec
Dmitry Marchikhin <diwanursa@gmail.com>
2016-11-21 10:57:09 +0200

    bug with error input fixed

--- js/register.js
+++ js/register.js
@@ -14,29 +14,42 @@ $(document).ready(function() {
         name: value
       },
       success: function(xhr, data, textStatus) {
+
         name.removeClass('sign-input-error');
         name.removeClass('sign-input-correct');
         nameError.html('');
         nameError.removeClass('sign-desc-active');
         nameError.removeClass('sign-desc-correct');
         nameError.removeClass('sign-desc-error');
+
         if (value.length === 0) {
+
           button.prop("disabled", true);
+
         } else if (xhr === 'no' && value.length >= 3) {
-          nameError.html('Correct');
+
+          name.addClass('sign-input-correct');
+
           nameError.addClass('sign-desc-active');
           nameError.addClass('sign-desc-correct');
-          name.hasClass('sign-input-correct');
+
+          nameError.html('Correct');
+
           button.prop("disabled", false);
+
         } else if (xhr === 'yes' || value.length < 3) {
+
+          name.addClass('sign-input-error');
+
           if (value.length < 3) {
             nameError.html('Name is too short');
           } else {
             nameError.html('User already exists');
           }
+
           nameError.addClass('sign-desc-active');
           nameError.addClass('sign-desc-error');
-          name.hasClass('sign-input-error');
+
           button.prop("disabled", true);
         } else {
           console.log('error');
@@ -58,18 +71,27 @@ $(document).ready(function() {
     passwordError.removeClass('sign-desc-error');
 
     if (value.length === 0) {
+
       button.prop("disabled", true);
+
     } else if (value.length >= 3) {
+
       password.addClass('sign-input-correct');
       passwordError.addClass('sign-desc-active');
       passwordError.addClass('sign-desc-correct');
+
       passwordError.html('Correct');
+
       button.prop("disabled", false);
+
     } else {
+
       password.addClass('sign-input-error');
       passwordError.addClass('sign-desc-active');
       passwordError.addClass('sign-desc-error');
+
       passwordError.html('Password is too short');
+
       button.prop("disabled", true);
     }
   });
